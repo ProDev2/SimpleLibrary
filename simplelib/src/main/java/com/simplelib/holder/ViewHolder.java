314eced2dd05b784c933cc51267b8dc8b23373b4
Devs <pascal.22.12.22@gmail.com>
2019-06-12 13:04:09 +0200

    Updated

--- simplelib/src/main/java/com/simplelib/holder/ViewHolder.java
+++ simplelib/src/main/java/com/simplelib/holder/ViewHolder.java
@@ -5,23 +5,61 @@ import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public abstract class ViewHolder {
     private Context context;
+    private ViewGroup parentView;
 
     private View contentView;
 
     public ViewHolder(Context context) {
         if (context == null)
-            throw new NullPointerException();
+            throw new NullPointerException("No context attached");
 
         this.context = context;
 
         create();
     }
 
+    public ViewHolder(View contentView) {
+        if (contentView == null)
+            throw new NullPointerException("No content view attached");
+
+        this.context = contentView.getContext();
+
+        if (context == null)
+            throw new NullPointerException("No context attached");
+
+        this.contentView = contentView;
+
+        create();
+    }
+
+    public ViewHolder(ViewGroup parentView) {
+        if (parentView == null)
+            throw new NullPointerException("No parent view attached");
+
+        this.context = parentView.getContext();
+
+        if (context == null)
+            throw new NullPointerException("No context attached");
+
+        this.parentView = parentView;
+
+        create();
+    }
+
     public final Context getContext() {
         return context;
+    }
+
+    public ViewGroup getParentView() {
+        return parentView;
+    }
+
+    public void setParentView(ViewGroup parentView) {
+        this.parentView = parentView;
     }
 
     public final View getContentView() {
@@ -39,9 +77,20 @@ public abstract class ViewHolder {
     }
 
     public View inflateLayout(@LayoutRes int id) {
+        return inflateLayout(id, true);
+    }
+
+    public View inflateLayout(@LayoutRes int id, boolean useParent) {
+        return inflateLayout(id, useParent, false);
+    }
+
+    public View inflateLayout(@LayoutRes int id, boolean useParent, boolean attachToParent) {
         try {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            return inflater.inflate(id, null);
+            if (parentView != null && useParent)
+                return inflater.inflate(id, parentView, attachToParent);
+            else
+                return inflater.inflate(id, null);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,22 +105,23 @@ public abstract class ViewHolder {
     public void create() {
         try {
             if (contentView == null)
-                contentView = createHolder();
+                contentView = createHolder(parentView);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         if (contentView == null)
-            throw new NullPointerException();
+            throw new NullPointerException("No content view attached");
 
         try {
-            bindHolder(contentView);
+            if (contentView != null)
+                bindHolder(contentView);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public abstract View createHolder();
+    public abstract View createHolder(ViewGroup parentView);
 
     public abstract void bindHolder(View contentView);
 }
