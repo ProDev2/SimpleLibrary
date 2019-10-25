e415245c64f3f19abec94cb31294e6cf6c7a07c1
Devs <pascal.22.12.22@gmail.com>
2019-10-25 18:19:41 +0200

    Migrated to AndroidX

--- simplelib/src/main/java/com/simplelib/ids/ID.java
+++ simplelib/src/main/java/com/simplelib/ids/ID.java
@@ -111,6 +111,44 @@ public final class ID {
         }
     }
 
+    @SuppressWarnings("unchecked")
+    public static <E> Pool.IdIterator<E> getIdIterator(String poolId) {
+        Pool pool = null;
+        try {
+            pool = getPool(poolId);
+        } catch (Throwable tr) {
+            tr.printStackTrace();
+        }
+        if (pool == null)
+            return null;
+        return pool.getIdIterator();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E> Pool.EntryIterator<E> getEntryIterator(String poolId) {
+        Pool pool = null;
+        try {
+            pool = getPool(poolId);
+        } catch (Throwable tr) {
+            tr.printStackTrace();
+        }
+        if (pool == null)
+            return null;
+        return pool.getEntryIterator();
+    }
+
+    public static void clear(String poolId) {
+        Pool pool = null;
+        try {
+            pool = getPool(poolId);
+        } catch (Throwable tr) {
+            tr.printStackTrace();
+        }
+        if (pool == null)
+            return;
+        pool.clear();
+    }
+
     public static boolean has(String poolId, int id) {
         Pool pool = null;
         try {
@@ -147,6 +185,10 @@ public final class ID {
         if (pool == null)
             return null;
         return pool.remove(id);
+    }
+
+    public static int next(String poolId) {
+        return next(poolId, null);
     }
 
     public static <E> int next(String poolId, E arg) {
@@ -191,7 +233,7 @@ public final class ID {
         return pool.removeAndInvoke(id);
     }
 
-    public static final class Pool<E> implements Iterable<Pool.Entry<E>> {
+    public static final class Pool<E> implements Iterable<Integer> {
         private final int min, max;
 
         private final List<Entry<E>> list;
@@ -214,33 +256,71 @@ public final class ID {
             this.lastCursor = 0;
         }
 
+        public synchronized int getMin() {
+            return min;
+        }
+
+        public synchronized int getMax() {
+            return max;
+        }
+
         @Override
-        public synchronized Iterator<Entry<E>> iterator() {
-            Iterator<Entry<E>> entryIterator = list.iterator();
+        public synchronized Iterator<Integer> iterator() {
+            return getIdIterator();
+        }
 
-            return new Iterator<Entry<E>>() {
-                @Override
-                public boolean hasNext() {
-                    synchronized (Pool.this) {
-                        return entryIterator != null ? entryIterator.hasNext() : null;
-                    }
-                }
+        public synchronized IdIterator<E> getIdIterator() {
+            return new IdIterator<E>(this);
+        }
 
-                @Override
-                public Entry<E> next() {
-                    synchronized (Pool.this) {
-                        return entryIterator != null ? entryIterator.next() : null;
-                    }
-                }
+        public synchronized EntryIterator<E> getEntryIterator() {
+            return new EntryIterator<>(this);
+        }
 
-                @Override
-                public void remove() {
-                    synchronized (Pool.this) {
-                        if (entryIterator != null)
-                            entryIterator.remove();
-                    }
-                }
-            };
+        public synchronized int calculateFirstId() {
+            int minId = Math.max(min, 0);
+            int maxId = Math.max(max < 0 ? lastCursor : Math.min(lastCursor, max), minId);
+
+            int id = Math.min(minId, maxId);
+            Entry<E> entry;
+
+            while (((entry = getEntry(id)) == null ||
+                    entry.isEmpty()) &&
+                    id >= minId &&
+                    id <= maxId)
+                id++;
+
+            if (entry != null &&
+                    !entry.isEmpty() &&
+                    hasId(id) &&
+                    id >= minId &&
+                    id <= maxId)
+                return id;
+
+            return -1;
+        }
+
+        public synchronized int calculateLastId() {
+            int minId = Math.max(min, 0);
+            int maxId = Math.max(max < 0 ? lastCursor : Math.min(lastCursor, max), minId);
+
+            int id = Math.max(minId, maxId);
+            Entry<E> entry;
+
+            while (((entry = getEntry(id)) == null ||
+                    entry.isEmpty()) &&
+                    id >= minId &&
+                    id <= maxId)
+                id--;
+
+            if (entry != null &&
+                    !entry.isEmpty() &&
+                    hasId(id) &&
+                    id >= minId &&
+                    id <= maxId)
+                return id;
+
+            return -1;
         }
 
         public synchronized void clear() {
@@ -311,8 +391,8 @@ public final class ID {
 
             cursor = id + 1;
             for (Entry<E> entry;
-                 (entry = getEntry(cursor)) != null
-                         && !entry.isEmpty() &&
+                 (entry = getEntry(cursor)) != null &&
+                         !entry.isEmpty() &&
                          (max < 0 || cursor <= max);
                  cursor++);
 
@@ -412,6 +492,135 @@ public final class ID {
 
             public interface Listener<E> {
                 void onInvoke(int id, E arg);
+            }
+        }
+
+        public static class PoolIterator<E> {
+            public static final int NO_ID = -1;
+
+            protected final Pool<E> pool;
+
+            private int cursor;
+            private boolean hasNext;
+
+            public PoolIterator(Pool<E> pool) {
+                if (pool == null)
+                    throw new NullPointerException("No pool attached");
+
+                this.pool = pool;
+
+                reset();
+            }
+
+            public synchronized final void reset() {
+                this.cursor = NO_ID;
+                this.hasNext = false;
+            }
+
+            public synchronized final void skip() {
+                synchronized (pool) {
+                    int minId = Math.max(pool.getMin(), 0);
+
+                    int nextCursor;
+                    if (cursor != NO_ID &&
+                            cursor >= minId) {
+                        int currentId = Math.max(Math.max(pool.getMin(), 0), cursor);
+                        nextCursor = findValidId(currentId + 1);
+                    } else {
+                        nextCursor = findValidId(minId);
+                    }
+
+                    if (nextCursor != NO_ID &&
+                            nextCursor >= minId) {
+                        hasNext = true;
+                        cursor = nextCursor;
+                    } else {
+                        hasNext = false;
+                    }
+                }
+            }
+
+            public synchronized final int findValidId(int cursor) {
+                synchronized (pool) {
+                    int minId = pool.calculateFirstId();
+                    int maxId = pool.calculateLastId();
+
+                    int id = Math.max(minId, cursor);
+                    Entry<E> entry;
+
+                    while (((entry = pool.getEntry(id)) == null ||
+                            entry.isEmpty()) &&
+                            id >= minId &&
+                            id <= maxId)
+                        id++;
+
+                    if (entry != null &&
+                            !entry.isEmpty() &&
+                            pool.hasId(id) &&
+                            id >= minId &&
+                            id <= maxId)
+                        return id;
+
+                    return NO_ID;
+                }
+            }
+
+            public synchronized final boolean hasNextId() {
+                synchronized (pool) {
+                    if (cursor == NO_ID || !hasNext)
+                        skip();
+                    return hasNext;
+                }
+            }
+
+            public synchronized final int nextId() {
+                synchronized (pool) {
+                    if (cursor == NO_ID || !hasNext)
+                        skip();
+
+                    int minId = Math.max(pool.getMin(), 0);
+                    int id = cursor >= minId ? cursor : NO_ID;
+
+                    if (id != NO_ID && hasNext) {
+                        skip();
+                        return id;
+                    }
+
+                    return NO_ID;
+                }
+            }
+        }
+
+        public static final class IdIterator<E> extends PoolIterator<E> implements Iterator<Integer> {
+            public IdIterator(Pool<E> pool) {
+                super(pool);
+            }
+
+            @Override
+            public boolean hasNext() {
+                return super.hasNextId();
+            }
+
+            @Override
+            public Integer next() {
+                return super.nextId();
+            }
+        }
+
+        public static final class EntryIterator<E> extends PoolIterator<E> implements Iterator<Entry<E>> {
+            public EntryIterator(Pool<E> pool) {
+                super(pool);
+            }
+
+            @Override
+            public boolean hasNext() {
+                return super.hasNextId();
+            }
+
+            @Override
+            public Entry<E> next() {
+                int id = super.nextId();
+                return pool.getEntry(id);
             }
         }
     }
