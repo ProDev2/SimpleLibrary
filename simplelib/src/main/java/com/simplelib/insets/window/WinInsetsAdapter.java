f0d5b35d5b6927de7b2ebec909c9b45336669632
Devs <pascal.22.12.22@gmail.com>
2020-12-07 11:42:10 +0100

    Updated copyright

--- simplelib/src/main/java/com/simplelib/insets/window/WinInsetsAdapter.java
+++ simplelib/src/main/java/com/simplelib/insets/window/WinInsetsAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 ProDev+ (Pascal Gerner).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.simplelib.insets.window;
 
 import android.os.Build;
@@ -16,7 +32,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public abstract class WinInsetsAdapter extends InsetsAdapter<WinInsets> {
     public static boolean USE_VIEW_TAG = false;
 
-    protected static final int VIEW_TAG_KEY = 167448252;
+    protected static final int VIEW_TAG_KEY;
+
+    static {
+        int vtk = 1678252;
+        VIEW_TAG_KEY = vtk | 2 << 24;
+    }
 
     @NonNull
     private final View view;
